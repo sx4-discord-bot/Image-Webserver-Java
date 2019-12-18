a9591b7f44ee4fa7d8ff648aea5cde28d14f555a
Shea4 <sc4gaming@gmail.com>
2019-12-18 22:34:59 +0000

    Changes to the map class

--- Webserver/src/main/java/com/sx4/webserver/image/Body.java
+++ Webserver/src/main/java/com/sx4/webserver/image/Body.java
@@ -1,61 +1,122 @@
 package com.sx4.webserver.image;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class Body extends HashMap<String, Object> {
-
-	private static final long serialVersionUID = 1L;
+public class Body implements Map<String, Object> {
 	
-	public Body() {}
+	private final Map<String, Object> map;
+	
+	public Body() {
+		this.map = new HashMap<>();
+	}
+	
+	public Body(Map<String, Object> map) {
+		this.map = Map.copyOf(map);
+	}
 	
 	public Body(String key, Object value) {
-		super.put(key, value);
+		this.map = new HashMap<>();
+		
+		this.map.put(key, value);
 	}
 	
 	public Body put(String key, Object value) {
-		super.put(key, value);
+		this.map.put(key, value);
+		
+		return this;
+	}
+	
+	public Body putAll(Body body) {
+		this.map.putAll(body);
 		
 		return this;
 	}
 	
 	@SuppressWarnings("unchecked")
-	public <Type> Type get(Object key, Type defaultValue) {
-		Object value = super.get(key);
+	public <Type> Type get(String key, Type defaultValue) {
+		Object value = this.map.get(key);
 		
 		return value == null ? defaultValue : (Type) value;
 	}
 	
-	public <Type> Type get(Object key, Class<Type> clazz) {
-		return clazz.cast(super.get(key));
+	public <Type> Type get(String key, Class<Type> clazz) {
+		return clazz.cast(this.map.get(key));
 	}
 	
-	public String getString(Object key) {
-		return (String) super.get(key);
+	public String getString(String key) {
+		return (String) this.map.get(key);
 	}
 	
-	public Long getLong(Object key) {
-		return (Long) super.get(key);
+	public Long getLong(String key) {
+		return (Long) this.map.get(key);
 	}
 	
-	public Integer getInteger(Object key) {
-		return (Integer) super.get(key);
+	public Integer getInteger(String key) {
+		return (Integer) this.map.get(key);
 	}
 	
-	public Boolean getBoolean(Object key) {
-		return (Boolean) super.get(key);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public <Type> List<Type> getList(Object key, Class<Type> clazz) {
-		return (List<Type>) super.get(key);
+	public Boolean getBoolean(String key) {
+		return (Boolean) this.map.get(key);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public <Type> List<Type> getList(Object key, Class<Type> clazz, List<Type> defaultValue) {
-		Object value = super.get(key);
+	public <Type> List<Type> getList(String key, Class<Type> clazz) {
+		return (List<Type>) this.map.get(key);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <Type> List<Type> getList(String key, Class<Type> clazz, List<Type> defaultValue) {
+		Object value = this.map.get(key);
 		
 		return value == null ? defaultValue : (List<Type>) value;
+	}
+
+	public int size() {
+		return this.map.size();
+	}
+
+	public boolean isEmpty() {
+		return this.map.isEmpty();
+	}
+
+	public boolean containsKey(Object key) {
+		return this.map.containsKey(key);
+	}
+
+	public boolean containsValue(Object value) {
+		return this.map.containsValue(value);
+	}
+
+	public Object get(Object key) {
+		return this.map.get(key);
+	}
+
+	public Object remove(Object key) {
+		return this.map.remove(key);
+	}
+
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		this.map.putAll(m);
+	}
+
+	public void clear() {
+		this.map.clear();
+	}
+
+	public Set<String> keySet() {
+		return this.map.keySet();
+	}
+
+	public Collection<Object> values() {
+		return this.map.values();
+	}
+
+	public Set<Entry<String, Object>> entrySet() {
+		return this.map.entrySet();
 	}
 
 }
